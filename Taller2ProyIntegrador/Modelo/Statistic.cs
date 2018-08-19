3eff38d8ff526a527ecfc749e6f1a29485e5e962
BeadierDark <35853208+BeadierDark@users.noreply.github.com>
2018-08-19 14:49:48 -0500

    avanzando para pullear

--- Taller2ProyIntegrador/Modelo/Statistic.cs
+++ Taller2ProyIntegrador/Modelo/Statistic.cs
@@ -8,5 +8,16 @@ namespace Modelo
 {
     class Statistic
     {
+        public static int YEAR_FOUNDED;
+        public static int GEN_RES_AREA;
+        public static int SPCRES_AREA;
+        public static int CATEGORY;
+        public static int CITY_NAME;
+        public static int STATE_NAME;
+        public static int REGION_NAME;
+
+        private Dictionary<String, List<ResearchGroup>>[] attributeCounter;
+
+
     }
 }
