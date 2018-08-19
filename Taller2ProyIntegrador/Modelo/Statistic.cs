e3e3309823ce42ac547eec609928b61f50e601ab
BeadierDark <35853208+BeadierDark@users.noreply.github.com>
2018-08-19 18:35:35 -0500

    Construsctor Statistics changed

--- Taller2ProyIntegrador/Modelo/Statistic.cs
+++ Taller2ProyIntegrador/Modelo/Statistic.cs
@@ -44,15 +44,80 @@ namespace Modelo
                 }
             }
                 attributeCounter[GEN_RES_AREA] = new Dictionary<string, List<ResearchGroup>>();
-
-            attributeCounter[3] = new Dictionary<string, List<ResearchGroup>>();
-
-            attributeCounter[4] = new Dictionary<string, List<ResearchGroup>>();
-
-            attributeCounter[5] = new Dictionary<string, List<ResearchGroup>>();
-
-            attributeCounter[6] = new Dictionary<string, List<ResearchGroup>>();
-
+            foreach (ResearchGroup x in groups)
+            {
+                if (attributeCounter[GEN_RES_AREA].ContainsKey(x.GeneralResearchArea))
+                {
+                    List<ResearchGroup> alv = attributeCounter[GEN_RES_AREA][x.GeneralResearchArea];
+                    alv.Add(x);
+                }
+                else
+                {
+                    List<ResearchGroup> alv = new List<ResearchGroup>();
+                    alv.Add(x);
+                    attributeCounter[GEN_RES_AREA].Add(x.GeneralResearchArea, alv);
+                }
+            }
+            attributeCounter[SPCRES_AREA] = new Dictionary<string, List<ResearchGroup>>();
+            foreach (ResearchGroup x in groups)
+            {
+                if (attributeCounter[SPCRES_AREA].ContainsKey(x.SpecificResearchArea))
+                {
+                    List<ResearchGroup> alv = attributeCounter[SPCRES_AREA][x.SpecificResearchArea];
+                    alv.Add(x);
+                }
+                else
+                {
+                    List<ResearchGroup> alv = new List<ResearchGroup>();
+                    alv.Add(x);
+                    attributeCounter[SPCRES_AREA].Add(x.SpecificResearchArea, alv);
+                }
+            }
+            attributeCounter[CITY_NAME] = new Dictionary<string, List<ResearchGroup>>();
+            foreach (ResearchGroup x in groups)
+            {
+                if (attributeCounter[CITY_NAME].ContainsKey(x.Location.CityName))
+                {
+                    List<ResearchGroup> alv = attributeCounter[CITY_NAME][x.Location.CityName];
+                    alv.Add(x);
+                }
+                else
+                {
+                    List<ResearchGroup> alv = new List<ResearchGroup>();
+                    alv.Add(x);
+                    attributeCounter[CITY_NAME].Add(x.Location.CityName, alv);
+                }
+            }
+            attributeCounter[STATE_NAME] = new Dictionary<string, List<ResearchGroup>>();
+            foreach (ResearchGroup x in groups)
+            {
+                if (attributeCounter[STATE_NAME].ContainsKey(x.Location.StateName))
+                {
+                    List<ResearchGroup> alv = attributeCounter[STATE_NAME][x.Location.StateName];
+                    alv.Add(x);
+                }
+                else
+                {
+                    List<ResearchGroup> alv = new List<ResearchGroup>();
+                    alv.Add(x);
+                    attributeCounter[STATE_NAME].Add(x.Location.StateName, alv);
+                }
+            }
+            attributeCounter[REGION_NAME] = new Dictionary<string, List<ResearchGroup>>();
+            foreach (ResearchGroup x in groups)
+            {
+                if (attributeCounter[REGION_NAME].ContainsKey(x.Location.RegionName))
+                {
+                    List<ResearchGroup> alv = attributeCounter[STATE_NAME][x.Location.RegionName];
+                    alv.Add(x);
+                }
+                else
+                {
+                    List<ResearchGroup> alv = new List<ResearchGroup>();
+                    alv.Add(x);
+                    attributeCounter[REGION_NAME].Add(x.Location.RegionName, alv);
+                }
+            }
         }
 
         public double ProbabilityOfAttAOccurringInB (int A, int B, String valA, String valB)
