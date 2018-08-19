9c6c4805d90b73b85b3c1a5baa9bf719629c4947
danielgalvis98 <dangaltor@gmail.com>
2018-08-18 23:48:16 -0500

    ResearchManager del Diagrama

--- Taller2ProyIntegrador/Modelo/ResearchManager.cs
+++ Taller2ProyIntegrador/Modelo/ResearchManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,7 @@ namespace Modelo
     class ResearchManager
     {
         public const string SERIALISABLE_PATH = "../../../docs/ResearchGroups";
+        public const string TXT_PATH = "../../../docs/txtInvestigacion.txt";
 
         private Statistic statistics;
         private ListSerialisable<ResearchGroup> researchGroups;
@@ -19,6 +22,7 @@ namespace Modelo
         {
             researchGroups = new ListSerialisable<ResearchGroup>();
             randomGenerator = new Random();
+            LoadResearchGroup();
 
         }
 
@@ -46,6 +50,69 @@ namespace Modelo
 
             
             return retorno;
+        }
+
+        public bool LoadResearchGroup()
+        {
+            bool retorno = false;
+
+            BinaryFormatter formateador = new BinaryFormatter();
+
+            Stream str = null;
+            try
+            {
+                str = new FileStream(SERIALISABLE_PATH, FileMode.Open, FileAccess.Read, FileShare.None);
+                researchGroups = (ListSerialisable<ResearchGroup>)formateador.Deserialize(str);
+                retorno = true;
+            } catch (FileNotFoundException e)
+            {
+                retorno = true;
+                var fileStream = File.OpenRead(TXT_PATH);
+                var streamReader = new StreamReader(fileStream, Encoding.UTF8, true);
+                string line = streamReader.ReadLine();
+                while ((line = streamReader.ReadLine())!= null && !line.Equals("") && retorno)
+                {
+                    string[] cityData = line.Split('\t');
+                    string grCode = cityData[2];
+                    string Date = cityData[4];
+                    string grName = cityData[3];
+                    string daneCode = cityData[9];
+                    string genResAre = cityData[12];
+                    string spResArea = cityData[11];
+                    string categ = cityData[13];
+                    string cn = cityData[5];
+                    string sn = cityData[6];
+                    string rn = cityData[8];
+
+                    retorno = RegisterResearchGroup(grCode, Date, grName, daneCode, genResAre, spResArea, categ, cn, sn, rn);
+                }
+
+                if (retorno)
+                {
+                    str = new FileStream(SERIALISABLE_PATH, FileMode.Create, FileAccess.Read, FileShare.None);
+                    formateador.Serialize(str, researchGroups);
+                }
+                fileStream.Close();
+                streamReader.Close();
+            }
+            str.Close();
+
+            return retorno;
+        }
+
+        public List<ResearchGroup> GetGroups(string [] atr, bool [] toCompare)
+        {
+            List<ResearchGroup> groups = new List<ResearchGroup>();
+            foreach (ResearchGroup gr in researchGroups)
+            {
+                if (gr.checkParameters(atr, toCompare))
+                {
+                    groups.Add(gr);
+                }
+            }
+
+
+            return groups;
         }
 
 
