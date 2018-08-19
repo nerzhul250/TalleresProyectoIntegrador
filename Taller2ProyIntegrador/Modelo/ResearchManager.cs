0777afdebbe25f0b9e6a088feb85c5ff360c03a0
danielgalvis98 <dangaltor@gmail.com>
2018-08-19 14:12:01 -0500

    DateTime en vez de String

--- Taller2ProyIntegrador/Modelo/ResearchManager.cs
+++ Taller2ProyIntegrador/Modelo/ResearchManager.cs
@@ -29,7 +29,7 @@ namespace Modelo
 
         }
 
-        public bool RegisterResearchGroup (string grCode, string Date, string grName, string daneCode, string genResAre, string spResAre,
+        public bool RegisterResearchGroup (string grCode, DateTime Date, string grName, string daneCode, string genResAre, string spResAre,
             string categ, string cn, string sn, string rn)
         {
             bool retorno = false;
@@ -40,7 +40,7 @@ namespace Modelo
             {
                 try
                 {
-                    ResearchGroup gr = new ResearchGroup(grCode, Convert.ToDateTime(Date), grName, daneCode, genResAre, spResAre, categ, randomGenerator);
+                    ResearchGroup gr = new ResearchGroup(grCode, Date, grName, daneCode, genResAre, spResAre, categ, randomGenerator);
                     gr.inicializateLocation(cn, rn, sn);
                     researchGroups.Add(gr);
                     retorno = true;
@@ -56,7 +56,7 @@ namespace Modelo
         }
 
 
-        public bool RegisterResearchGroup(string grCode, string Date, string grName, string daneCode, string genResAre, string spResAre,
+        public bool RegisterResearchGroup(string grCode, DateTime Date, string grName, string daneCode, string genResAre, string spResAre,
            string categ, string cn, string sn, string rn, double lat, double lng)
         {
             bool retorno = false;
@@ -67,7 +67,7 @@ namespace Modelo
             {
                 try
                 {
-                    ResearchGroup gr = new ResearchGroup(grCode, Convert.ToDateTime(Date), grName, daneCode, genResAre, spResAre, categ, randomGenerator);
+                    ResearchGroup gr = new ResearchGroup(grCode, Date, grName, daneCode, genResAre, spResAre, categ, randomGenerator);
                     gr.inicializateLocation(cn, rn, sn, lat, lng);
                     researchGroups.Add(gr);
                     retorno = true;
@@ -124,7 +124,7 @@ namespace Modelo
                     double lat = gmap.Position.Lat;
                     double lng = gmap.Position.Lng;
 
-                    retorno = RegisterResearchGroup(grCode, Date, grName, daneCode, genResAre, spResArea, categ, cn, sn, rn, lat, lng);
+                    retorno = RegisterResearchGroup(grCode, Convert.ToDateTime(Date), grName, daneCode, genResAre, spResArea, categ, cn, sn, rn, lat, lng);
                 }
 
                 if (retorno)
